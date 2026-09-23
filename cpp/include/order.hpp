94cef7bada62789b4ca8f1ff888ddbece20baadf
Andrew Kaminer <andrewkaminer@gmail.com>
2026-09-23 10:13:56 -0400

    Move to uintptr_t id type

--- cpp/include/order.hpp
+++ cpp/include/order.hpp
@@ -8,7 +8,7 @@ enum class OrderSide {
 };
 
 struct Order {
-  unsigned long id {reinterpret_cast<std::uintptr_t>(this)}; // use memory address as unique id
+  std::uintptr_t id {reinterpret_cast<std::uintptr_t>(this)}; // use memory address as unique id for the aura
   OrderSide side {OrderSide::BUY};
   unsigned int price {0};
   unsigned int quantity {0};
